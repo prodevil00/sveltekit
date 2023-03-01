927b2aaa7487fd14255dc034da8b229e706bb9ed
ankittchaurasia <74493798+ankittchaurasia@users.noreply.github.com>
2023-03-01 11:36:19 +0530

    1 file modified

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-cloudflare';
 
 // import { middleware } from './src/middleware.js';
 
